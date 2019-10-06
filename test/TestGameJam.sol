1639a34fb66e8f907f7b67b38a8a5174bdf89858
Ryan Gordon <ryangordon210@gmail.com>
2019-10-06 20:02:10 +0100

    another 2 tests
    testFinishCantBeDoneTwice
    and
    testPayoutWinnerOnDeployedContract

--- test/TestGameJam.sol
+++ test/TestGameJam.sol
@@ -7,20 +7,20 @@ import "../contracts/GameJam.sol";
 contract TestGameJam {
   // Truffle will send the TestGameJam one Ether after deploying the contract.
   uint public initialBalance = 1 ether;
-
+  GameJam gameJam;
+  function beforeEach() public {
+      gameJam = GameJam(DeployedAddresses.GameJam());
+  }
   function testInitialBalanceUsingDeployedContract() public {
-    GameJam gameJam = GameJam(DeployedAddresses.GameJam());
     uint expected = 0;
     Assert.equal(address(gameJam).balance, expected, "Initial Balance should be zero");
   }
 
   function testInitialStageUsingDeployedContract() public {
-    GameJam gameJam = GameJam(DeployedAddresses.GameJam());
     require(gameJam.stage() == GameJam.Stages.JamInProgress, "Initial Stage should be JamInProgress");
   }
 
   function testPayoutDoesNotWorkAtInitialState() public {
-    GameJam gameJam = GameJam(DeployedAddresses.GameJam());
     address payable potentialWinner = address(uint160(address(this))); // Cast to an address payable
     bool r;
     
@@ -32,10 +32,22 @@ contract TestGameJam {
   }
  
   function testFinishingTheDeployedContract() public {
-    GameJam gameJam = GameJam(DeployedAddresses.GameJam());
     require(gameJam.stage() == GameJam.Stages.JamInProgress, "Stage should be JamInProgress before finish");
     gameJam.finish.value(0)(address(this));
     require(gameJam.stage() == GameJam.Stages.WinnerDeclaration, "Stage should be WinnerDeclaration after finish");
   }
 
+  function testFinishCantBeDoneTwice() public {
+    bool r;
+    require(gameJam.stage() != GameJam.Stages.JamInProgress, "Stage should not be JamInProgress");
+    (r, ) = address(gameJam).call(abi.encodePacked(gameJam.finish.selector, address(this)));
+    Assert.isFalse(r, "Call to finish should be unsuccessful when stage is not JamInProgress");
+  }
+
+  function testPayoutWinnerOnDeployedContract() public {
+    address payable potentialWinner = address(uint160(address(this))); // Cast to an address payable
+    require(gameJam.stage() == GameJam.Stages.WinnerDeclaration, "Stage should be WinnerDeclaration");
+    gameJam.payoutWinner.value(0)(potentialWinner);
+    require(gameJam.stage() == GameJam.Stages.Finished, "Stage should be Finished");
+    }
 }
