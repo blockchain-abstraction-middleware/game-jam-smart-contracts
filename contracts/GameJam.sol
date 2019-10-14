b519244a136d605447c8f2a5a5a6a47c5d64fe99
Ryan Gordon <ryangordon.dev@gmail.com>
2019-10-14 13:30:16 +0100

    Initial code for RBAC (#3)
    * Initial code for RBAC
    
    Involves holding a number of admin Roles which can be expanded for judge roles also.
    
    the starting and finishing of the competition is restricted to those with the admin role and so is the winnerPayout.
    Added new tests; this and existing tests means Roles are tested
    * Formatted test file and Wrote some more Javascript tests
    Tested payables in Remix
    Refactored require to a simpler syntax.
    Moved event to only fire if something is sent on payout.
    
    * Fix import for openzepplin
    
    No longer import from node_modules

--- contracts/GameJam.sol
+++ contracts/GameJam.sol
@@ -1,15 +1,23 @@
 pragma solidity 0.5.11;
 
+import "openzeppelin-solidity/contracts/access/Roles.sol";  //Import Roles to implement custom Role Based Access Control
+
 contract GameJam {
+  using Roles for Roles.Role;
+
   enum Stages {
         Registration,
         InProgress,
         Finished
     }
   uint public balance;
+  
 
   //Set the initial Stage
   Stages public stage = Stages.Registration;
+
+  //Define the roles
+  Roles.Role private admins;
 
   // Modifier used to restrict access unless a given Stage is active
   modifier onlyAtStage(Stages _stage) {
@@ -32,8 +40,26 @@ contract GameJam {
       nextStage();
   }
 
+  //Role Setup
+  // On Deploy, the sender of the transaction will be added as a admin
+  // Further admins could be added with a Role restricted _addGameJamAdmin() function
+  //Modifier for Admins
+  modifier onlyGameJamAdmin() {
+      require(isGameJamAdmin(msg.sender), "GameJamAdmin role required: caller does not have the GameJamAdmin role");
+      _;
+  }
+  // Function to check that the address is a valid admin
+  function isGameJamAdmin(address account) public view returns (bool) {
+        return admins.has(account);
+  }
+  function _addGameJamAdmin(address account) internal {
+    admins.add(account);
+    emit GameJamAdminAdded(account);
+  }
+
   mapping(address => string) public competitors;
 
+  event GameJamAdminAdded(address indexed account);
   event CompetitorAdded(address competitor);
   event GameJameStarted(uint startTime);
   event WinnerDeclared(address winner);
@@ -43,6 +69,7 @@ contract GameJam {
   // which will be the amount to be distributed to winners
   constructor(uint _balance) public payable {
     balance = _balance;
+    _addGameJamAdmin(msg.sender);
   }
 
   function addCompetitor(
@@ -52,6 +79,7 @@ contract GameJam {
     public
     onlyAtStage(Stages.Registration)
   {
+    require(bytes(ipfsHash).length == 46, "incorrect length");
     competitors[competitor] = ipfsHash;
 
     emit CompetitorAdded(competitor);
@@ -60,32 +88,38 @@ contract GameJam {
   // finish function is a payable used to declare the winner via their address
   // the function should only be called able when the GameJam is in Registration
   function start() public payable
-    onlyAtStage(Stages.Registration)
+    onlyGameJamAdmin //Only an admin can start the GameJam
+    onlyAtStage(Stages.Registration) //GameJam can only be start when in Registration State
     transitionNext {
-
       emit GameJameStarted(now);
     }
 
   // finish function is a payable used to declare the winner via their address
   // the function should only be called able when the GameJam is in Progress
   function finish(address winner) public payable
+    onlyGameJamAdmin //Only an admin can finish the GameJam
     onlyAtStage(Stages.InProgress)
     transitionNext {
-
-
+      //Ensure the winner is a registered competitor. The mapping result is initialised "" so if winner is not in competitors then 0x0 is returned
+      require(
+        bytes(competitors[winner]).length != 0,
+        "Winner should be a registered competitor"
+        );
       emit WinnerDeclared(winner);
     }
 
   // payoutWinner is used after the Jam to payout the declared winner
   // and ultimately finish the GameJam.
   function payoutWinner(address payable winner) public payable
+    onlyGameJamAdmin //Only an admin can start the GameJam
     onlyAtStage(Stages.Finished) {
-      emit GameJamFinished(winner);
+      
       // If there are prizes to distribute
       if (balance > 0 && address(this).balance >= balance) {
         //Send the winner their prize and progress the state to Finished Stage
         winner.transfer(balance);
         balance = 0;
+        emit GameJamFinished(winner);
       }
   }
 }
--- test/TestGameJam.sol
+++ test/TestGameJam.sol
@@ -7,22 +7,20 @@ import "../contracts/GameJam.sol";
 contract TestGameJam {
   // Truffle will send the TestGameJam one Ether after deploying the contract.
   uint public initialBalance = 1 ether;
-  GameJam gameJam =  GameJam(DeployedAddresses.GameJam());
-  
-  function beforeEach() public {
-    gameJam = GameJam(DeployedAddresses.GameJam());
+  //Rather than use the GameJam project deployed by Migrations, create a new one, making this address the admin
+  GameJam gameJam = new GameJam(1);
+  GameJam deployedGameJam = GameJam(DeployedAddresses.GameJam());
 
-  }
+  string validIpfsHash = "QmPXgPCzbdviCVJTJxvYCWtMuRWCKRfNRVcSpARHDKFSha";
+
   function testInitialBalanceUsingDeployedContract() public {
     uint expected = 1;
-    
     Assert.equal(gameJam.balance(), expected, "Initial Balance should be zero");
   }
 
   function testInitialBalanceUsingNewContract() public {
     GameJam gameJamNew = new GameJam(0);
     uint expected = 0;
-    
     Assert.equal(gameJamNew.balance(), expected, "Initial Balance should be zero");
   }
 
@@ -40,13 +38,28 @@ contract TestGameJam {
     (r, ) = address(gameJam).call(abi.encodePacked(gameJam.payoutWinner.selector, potentialWinner));
     Assert.isFalse(r, "Call to payoutWinner should be unsuccessful when stage is InProgress");
   }
+  function testAddingThisAddressAsACompetitor() public {
+    require(gameJam.stage() == GameJam.Stages.Registration, "Stage should be Registration before start");
+    gameJam.addCompetitor(address(this), validIpfsHash);
+  }
  
-   function testStartingTheDeployedContract() public {
-        require(gameJam.stage() == GameJam.Stages.Registration, "Stage should be Registration before start");
-        gameJam.start.value(0)();
-        require(gameJam.stage() == GameJam.Stages.InProgress, "Stage should be InProgress after start");
-    }
+  function testStartingTheDeployedContract() public {
+    require(gameJam.stage() == GameJam.Stages.Registration, "Stage should be Registration before start");
+    gameJam.start.value(0)();
+    require(gameJam.stage() == GameJam.Stages.InProgress, "Stage should be InProgress after start");
+  }
 
+
+  function testFinishCantBeCalledWithANonRegisteredCompetitor() public {
+    require(gameJam.stage() == GameJam.Stages.InProgress, "Stage should be Registration");
+    bool r;
+    // Make a call to finish the GameJam from this address, the admin
+    // sends the address of the deployedGameJam contract as a winner
+    // this address hasn't been registered as a competitor and the call should fail
+    (r, ) = address(gameJam).call(abi.encodePacked(gameJam.finish.selector, address(deployedGameJam)));
+    Assert.isFalse(r, "Call to finish on the deployed contract should be unsuccessful as the winner isin't in the list");
+
+  }
   function testFinishingTheDeployedContract() public {
     require(gameJam.stage() == GameJam.Stages.InProgress, "Stage should be JamInProgress before finish");
     gameJam.finish.value(0)(address(this));
@@ -64,7 +77,19 @@ contract TestGameJam {
     address payable potentialWinner = address(uint160(address(this))); // Cast to an address payable
     require(gameJam.stage() == GameJam.Stages.Finished, "Stage should be Finished");
     gameJam.payoutWinner.value(0)(potentialWinner);
+  }
 
-    }
+  function testRoleBlocksStartingTheDeployedContract() public {
+    require(deployedGameJam.stage() == GameJam.Stages.Registration, "Stage should be Registration");
+    bool r;
+    
+    // We're basically calling our contract externally with a raw call, and using only the boolean
+    // value associated with the message call's success
+    // At this stage, before the JS tests have run, deployedGameJam should still be in Registration (asserted above)
+    // and we can try to start eh contract from THIS contract, which should fail as this contract isin't an admin.
+    (r, ) = address(gameJam).call(abi.encodePacked(deployedGameJam.start.selector, ""));
+    Assert.isFalse(r, "Call to start on the deployed contract should be unsuccessful as it was deployed during Migrations");
+  }
 
+  
 }
