b0d8c22f9558ec36a11b309fc2b3d27de406b0cf
Ryan Gordon <ryangordon210@gmail.com>
2019-10-17 22:43:28 +0100

    Created a GameJamManagerContract
    A GameJamManager is a contract which tracks 1 or more GameJams.
    
    PR Features:
    + Allows a manager to create a new GameJam
    + RBAC for main operations
    + Interfaces used for Contract Registry
    + Updated migrations to use new contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,22 @@
 var GameJam = artifacts.require("./GameJam.sol");
+var GameJamManager = artifacts.require("./GameJamManager.sol");
+var ContractRegistry = artifacts.require("./ContractRegistry.sol");
+
 
 module.exports = async (deployer, network) => {
   console.log(`deploying on ${network} network`)
 
   const initialBalance = 1
   await deployer.deploy.apply(deployer, [GameJam, initialBalance])
+  await deployer.deploy.apply(deployer, [ContractRegistry])
+
+  const contractRegistry = await ContractRegistry.deployed()
+  const gameJamContract = await GameJam.deployed()
+
+  await deployer.deploy.apply(deployer, [GameJamManager , contractRegistry.address])
+
+  const gameJamManager = await GameJamManager.deployed()
+
   console.log(`deployed on ${network} network`)
+  console.log(`GameJamManagers address: ${gameJamManager.address}`)
 };
